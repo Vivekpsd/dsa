fb4e8eb27714727cd66dbf3ccf803f91d0d8d7c5
Vivek Prasad <Vivekpsd01@gmail.com>
2023-10-14 11:28:45 +0530

    Binary Search Algorithm

---
+++ BinarySearch/src/BSAlgorithm.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BSAlgorithm {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int[] eles = {1, 2, 5, 6, 8, 11, 14, 19, 22, 25, 27};
+        int size = eles.length;
+        int min = 0;
+        int max = size - 1;
+        int target;
+        System.out.println("Enter Target Element: ");
+        target = scn.nextInt();
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (eles[mid] == target) {
+                System.out.println("Present at index: " + mid);
+                return;
+            }
+
+            if (target < eles[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        System.out.println("Element not present in array..");
+    }
+}
