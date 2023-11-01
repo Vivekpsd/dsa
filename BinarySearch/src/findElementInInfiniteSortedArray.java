16dacccdb648e85adf829876e228d174b6a1c82c
Vivek Prasad <Vivekpsd01@gmail.com>
2023-11-01 23:19:00 +0530

    BST - findElementInInfiniteSortedArray

--- BinarySearch/src/findElementInInfiniteSortedArray.java
+++ BinarySearch/src/findElementInInfiniteSortedArray.java
@@ -6,34 +6,34 @@ public class findElementInInfiniteSortedArray {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int nums[] = {1, 5, 7, 9, 10, 20, 30, 35, 40, 44, 56, 66, 72, 75, 77, 80, 85, 90, 99, 102, 112, 120, 140, 150, 155};
+        int flag = 0;
+
+        System.out.println("Enter target to search: ");
+        int target = scn.nextInt();
         int st = 0;
         int en = 1;
-        System.out.println("Enter target to search for: ");
-        int target = scn.nextInt();
-
-        while (target > nums[en]) {
-            int temp = en + 1;
-            en = en + (en - st + 1) * 2;
-            st = temp;
+        while(flag == 0) {
+            flag = search(nums, target, st, en);
+            st = en + 1;
+            en = st*2;
+            if (en > nums.length) {
+                System.out.println("Not found in array :(");
+                break;
+            }
         }
-        int ans = search(nums, target, st, en);
-        System.out.println("At index: " + ans);
-
+        System.out.println("Element found at index " + flag);
     }
 
     static int search(int[] nums, int target, int st, int en) {
-
-        while(st <= en) {
-            int mid = st + (en - st) / 2;
-
-            if (target < nums[mid]) {
+        while (st <= en) {
+            int mid = st + (en-st) / 2;
+            if (target == nums[mid]) return mid;
+            else if (target < nums[mid]) {
                 en = mid - 1;
-            } else if (target > nums[mid]) {
-                st = mid + 1;
             } else {
-                return mid;
+                st = mid + 1;
             }
         }
-        return -1;
+        return 0;
     }
 }
