3b754965655f9e0788c13a29e077d1389fb507fd
Vivek Prasad <Vivekpsd01@gmail.com>
2023-11-16 22:16:43 +0530

    LeetCode_287 (Find Duplicate) updated ...

--- Sorting/src/LeetCode_287.java
+++ Sorting/src/LeetCode_287.java
@@ -6,22 +6,45 @@ public class LeetCode_287 {
     //https://leetcode.com/problems/find-the-duplicate-number/description/
 
     public static void main(String[] args) {
-        int[] nums = {1,3,4,2,2};
+        int[] nums = {8,7,1,10,17,15,18,11,16,9,19,12,5,14,3,4,2,13,18,18};
 
         int duplicateNum = findDuplicate(nums);
         System.out.println("Dulicate num is: " + duplicateNum);
     }
-    public static int findDuplicate(int[] nums) {
-        int[] helper = new int[nums.length];
-        Arrays.fill(helper, -1);
 
-        for (int i = 0; i < nums.length; i++) {
-            int originalIdx = nums[i] - 1;
-            if (helper[originalIdx] == -1)
-                helper[originalIdx] = 0;
-            else
-                return nums[i];
+
+
+    // solving without using constant extra space . . .
+    public static int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i+1) {
+                int correctIdx = nums[i] - 1;
+                if (nums[i] != nums[correctIdx]) {
+                    int temp = nums[i];
+                    nums[i] = nums[correctIdx];
+                    nums[correctIdx] = temp;
+                } else {
+                    return nums[i];
+                }
+            } else  {
+                i++;
+            }
         }
         return -1;
     }
+
+//    public static int findDuplicate(int[] nums) {
+//        int[] helper = new int[nums.length];
+//        Arrays.fill(helper, -1);
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            int originalIdx = nums[i] - 1;
+//            if (helper[originalIdx] == -1)
+//                helper[originalIdx] = 0;
+//            else
+//                return nums[i];
+//        }
+//        return -1;
+//    }
 }
